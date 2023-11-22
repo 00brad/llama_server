e56fc28465f8c23e3dfcdf2ce00225879c041ec5
00Brad <46579244+00brad@users.noreply.github.com>
2023-11-22 17:26:50 -0500

    run detached

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker build -t llama-server .
 
 # Adjust timeout and max workers as needed
-docker run -p 80:80 -e MAX_WORKERS="1" -e TIMEOUT="600" llama-server
+docker run -d -p 80:80 -e MAX_WORKERS="1" -e TIMEOUT="600" llama-server
